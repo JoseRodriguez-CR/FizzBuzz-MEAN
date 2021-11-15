95d8070693286df3c392ef9b5c9ff0ca934a2cc3
JoseRodriguez-CR <josemariorodriguezc@gmail.com>
2021-11-15 11:03:28 -0600

    Uploading Files

---
+++ FizzBuzz.js
@@ -0,0 +1,21 @@
+function fizzbuzz (num) {
+
+    for (var i=1; i <= num; i++){
+
+        if((i%5 == 0) && (i%3 == 0)){
+            console.log(i + " FizzBuzz");
+        }
+        else if(i%3 == 0){
+            console.log(i + " Fizz");
+        }
+        else if(i%5 == 0){
+            console.log(i + " Buzz");
+        }
+        else
+        console.log(i);
+
+
+    }
+}
+
+fizzbuzz(150);
